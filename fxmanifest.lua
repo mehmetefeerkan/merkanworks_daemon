bd6ddf484dec0b84970e0780ef053fbdd4a550cb
mehmetefeerkan <mehmetefe@prodigygaming.network>
2022-01-17 10:49:24 +0300

    Fix fxmanifest

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,9 +1,10 @@
--- Resource Metadata
-fx_version 'cerulean'
-games { 'gta5' }
-
-author 'ephesus'
-description 'MerkanWorks Server Management Panel Daemon for FXServer'
 version '1.0.0'
+author 'ephesus'
+description 'Daemon for MEWSEMP'
+repository 'https://github.com/mehmetefeerkan'
 
 server_script 'server.js'
+
+fx_version 'adamant'
+games { 'gta5', 'rdr3' }
+rdr3_warning 'I acknowledge that this is a prerelease build of RedM, and I am aware my resources *will* become incompatible once RedM sh$
