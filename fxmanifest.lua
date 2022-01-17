8ccf97e3bb1614dbbc88c35d15899f00a6465be0
mehmetefeerkan <mehmetefe@prodigygaming.network>
2022-01-17 10:52:35 +0300

    uh

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -7,4 +7,4 @@ server_script 'server.js'
 
 fx_version 'adamant'
 games { 'gta5', 'rdr3' }
-rdr3_warning 'I acknowledge that this is a prerelease build of RedM, and I am aware my resources *will* become incompatible once RedM sh$
+rdr3_warning 'I acknowledge that this is a prerelease build of RedM, and I am aware my resources *will* become incompatible once RedM sh$'
